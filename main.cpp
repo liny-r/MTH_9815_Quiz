52c9db708bf806835933f0b8b9506d56d923c2cc
Rose Lin <yueqilin2000@gmail.com>
2025-11-25 17:49:41 -0500

    Merge remote-tracking branch 'origin/master'

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main() {
     Hashtable<int, std::string> table(10, h, eq);
 
     table.insert(1, "hello");
-    table.insert(2, "world");
+    table.insert(2, "you");
 
     std::cout << table.at(1) << std::endl;
     std::cout << table.at(2) << std::endl;
