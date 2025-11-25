473a3c995cf454caef96c36b2e9d34cd0b78069b
Rose Lin <yueqilin2000@gmail.com>
2025-11-25 18:08:46 -0500

    Merge remote-tracking branch 'origin/master'

--- main.cpp
+++ main.cpp
@@ -1,18 +1,8 @@
 #include <iostream>
-#include "Hashtable.hpp"
 
 
 int main() {
-    IntHasher h;
-    IntEquality eq;
-
-    Hashtable<int, std::string> table(10, h, eq);
-
-    table.insert(1, "hello");
-    table.insert(2, "you");
-
-    std::cout << table.at(1) << std::endl;
-    std::cout << table.at(2) << std::endl;
+    printf("Hello World\n");
 
     return 0;
 }
